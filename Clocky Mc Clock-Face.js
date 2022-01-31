1fd2542b95deebe54532160b5f54804c8b4b93bc
AndriiMshk <90900795+AndriiMshk@users.noreply.github.com>
2022-01-31 22:47:35 +0200

    Clocky Mc Clock-Face

---
+++ Clocky Mc Clock-Face.js
@@ -0,0 +1,11 @@
+var whatTimeIsIt = function(angle) {
+    let hour = Math.floor(angle / 30) 
+    let min = Math.floor((angle % 30) * 2);
+    if (hour < 10) {
+        hour = "0"+ hour;}
+    if (hour <= 0) {
+        hour = 12;}
+    if (min < 10) {
+        min = "0" + min;}
+      return hour + ":" + min;
+    }
